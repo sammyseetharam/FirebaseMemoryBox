a382a25ac261bfbf3a816e03bb8cb3f8c83c53e3
sammyseetharam <90780132+sammyseetharam@users.noreply.github.com>
2022-10-03 10:59:37 -0500

    commit

---
+++ app/src/main/java/com/example/mymemorybox/Memory.java
@@ -0,0 +1,89 @@
+package com.example.mymemorybox;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Memory implements Parcelable{
+
+    private int rating;
+    private String name;
+    private String desc;
+    private int imageResourceId;
+
+    public Memory(int rating, String name, String desc, int imageResourceId) {
+        this.rating = rating;
+        this.name = name;
+        this.desc = desc;
+        this.imageResourceId = imageResourceId;
+    }
+
+    public Memory(int rating, String name, String desc) {
+        this.rating = rating;
+        this.name = name;
+        this.desc = desc;
+        this.imageResourceId = 0;
+    }
+
+    public Memory() {
+        this.rating = 0;
+        this.name = "";
+        this.desc = "";
+        this.imageResourceId = 0;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+
+    public void setImageResourceId(int imageResourceId) {
+        this.imageResourceId = imageResourceId;
+    }
+
+    protected Memory(Parcel in) {
+    }
+
+    public static final Creator<Memory> CREATOR = new Creator<Memory>() {
+        @Override
+        public Memory createFromParcel(Parcel in) {
+            return new Memory(in);
+        }
+
+        @Override
+        public Memory[] newArray(int size) {
+            return new Memory[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+    }
+}
---
+++ app/src/main/java/com/example/mymemorybox/SignInActivity.java
@@ -0,0 +1,14 @@
+package com.example.mymemorybox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in_activity);
+    }
+}
